7280eee00f6a35208a168f8fb1a5a02c35f92d61
Nico Shanstrom <nicoshanstrom@gmail.com>
2024-06-25 19:38:03 -0400

    Comments line 87 out in rails_helper.rb for VCR

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -84,7 +84,7 @@ end
 VCR.configure do |config|
   config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   config.hook_into :webmock
-  config.filter_sensitive_data('<API_NAME>') { Rails.application.credentials.API_NAME[:API_KEY] }
+  # config.filter_sensitive_data('<API_NAME>') { Rails.application.credentials.API_NAME[:API_KEY] }
   config.configure_rspec_metadata!
   config.allow_http_connections_when_no_cassette = true
 end
